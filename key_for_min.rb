7e072a8ac397f1d699494e620853f0aaf4bb577f
Francisco Costa <francisco@fcosta.pt>
2019-03-18 14:22:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,11 @@
 
 
 def key_for_min_value(name_hash)
-  min_name = nil
-  min_value = nil
+  min_name = 0
+  min_value = 0
 
   name_hash.each do |name, value|
-    if min_value == nil || value < min_value
+    if min_value == 0 || value < min_value
       min_name = name
       min_value = value
     end
